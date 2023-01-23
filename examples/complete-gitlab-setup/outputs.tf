1d4354b5832000ab3197355deda6789007efeea9
Premdeep Saini <premdeep.saini@vedantu.com>
2023-01-23 09:12:43 +0530

    add example for gitlab omnibus setup using terraform-aws-gitlab module

---
+++ examples/complete-gitlab-setup/outputs.tf
@@ -0,0 +1,13 @@
+output "gitlab_pg_address" {
+  value       = module.gitlab.gitlab_pg_address
+  description = "Gitlab Postgres address"
+}
+
+output "gitlab_redis_address" {
+  value       = module.gitlab.gitlab_redis_address
+  description = "Gitlab Redis address"
+}
+
+output "gitlab_url" {
+  value = module.gitlab.gitlab_complete_url
+}
---
+++ examples/complete-gitlab-setup/variables.tf
@@ -0,0 +1,4 @@
+variable "private_key" {
+  type        = string
+  description = "Private key to execute ansible playbook on Gitlab instance."
+}
