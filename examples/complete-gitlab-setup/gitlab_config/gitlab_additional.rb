ea409077634d6e2ccccc4a54441c048f9b07b8f1
Premdeep Saini <premdeep.saini@vedantu.com>
2023-01-23 12:05:59 +0530

    add google oauth config to gitlab_additional.rb

--- examples/complete-gitlab-setup/gitlab_config/gitlab_additional.rb
+++ examples/complete-gitlab-setup/gitlab_config/gitlab_additional.rb
@@ -1,1 +1,8 @@
-
+gitlab_rails['omniauth_providers'] = [
+  {
+    name: "google_oauth2",
+    app_id: "google_oauth_app_id",
+    app_secret: "google_oauth_app_secret",
+    args: { access_type: "offline", approval_prompt: "" }
+  }
+]
